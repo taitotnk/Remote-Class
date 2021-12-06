de6f4d6ddecea4be163ecaa676905413196e732a
ryuga <s1f101900903@iniad.org>
2021-12-06 22:42:19 +0900

    fix intro page

--- src/components/IntroPage.tsx
+++ src/components/IntroPage.tsx
@@ -1,36 +1,44 @@
 import cssIntroPage from '../styles/IntroPage.module.css'
-import bg1 from "../images/login_img/login1.png"
-import bg2 from "../images/login_img/login2.png"
-import bg3 from "../images/login_img/login3.png"
-import bg4 from "../images/login_img/login4.png"
-import bg7 from "../images/login_img/login7.png"
-import scroll from "../images/login_img/scroll.png"
-import loginButton from "../images/login_img/loginbutton.png"
-import underLoginButton from "../images/login_img/under_login_button.png"
 import login from "lib/login";
+import Image from "next/image";
 
 import { Link as Scroll } from 'react-scroll';
 
 
 
-const IntroPage = () => {
+const IntroPage = (props) => {
     return (
         <>
-        <div className={cssIntroPage.bg1Box}>
-        <img src={bg1.src} className={cssIntroPage.bg + " " + cssIntroPage.topPage}/>
-        <img src={loginButton.src} className={cssIntroPage.loginButton} onClick={login}/>
-        <Scroll to="bg2" smooth={true}><img src={scroll.src} className={cssIntroPage.scroll}/></Scroll>
+        <div style={{position: 'relative', width:'100%', backgroundColor: 'red'}}>
+        <Image src="/images/login_img/page1.png" alt="page1" layout='fill' objectFit='contain' />
         </div>
 
-        <img src={bg2.src} className={cssIntroPage.bg} id="bg2"/>
-        <img src={bg3.src} className={cssIntroPage.bg} id="bg3"/>
-        <img src={bg4.src} className={cssIntroPage.bg} id="bg4"/>
-
-        <div className={cssIntroPage.bg7Box}>
-        <img src={bg7.src} className={cssIntroPage.bg} id="bg7"/>
-        <img src={underLoginButton.src} className={cssIntroPage.underLoginButton} onClick={login} />
+        <div style={{position: 'relative', width: '100%', height:'100hv', backgroundColor: 'red'}}>
+        <Image src="/images/login_img/page2.png" alt="page2" layout='fill' objectFit='contain' />
         </div>
+
+        <div style={{position: 'relative', width: '1920', height:'1080', backgroundColor: 'red'}}>
+        <Image src="/images/login_img/page3.png" alt="page3" layout='fill' objectFit='contain'/>
+        </div>
+
+        <div style={{position: 'relative', width: '1920', height:'1080', backgroundColor: 'red'}}>
+        <Image src="/images/login_img/page4.png" alt="page4" layout='fill'objectFit='contain'/>
+        </div>
+
+        <div style={{position: 'relative', width: '100%', height:'100vh', backgroundColor: 'red'}}>
+        <Image src="/images/login_img/page5.png" alt="page5" layout='fill'/>
+        </div>
+        
+        <div style={{position: 'relative', width: '100%', height:'100vh', backgroundColor: 'red'}}>
+        <Image src="/images/login_img/page6.png" alt="page6" layout='fill'/>
+        </div>
+
+        <div style={{position: 'relative', width: '100%', height:'100vh', backgroundColor: 'red'}}>
+        <Image src="/images/login_img/page7.png" alt="page7" layout='fill'/>
+        </div>
+        
         </>
+
     )
 }
 

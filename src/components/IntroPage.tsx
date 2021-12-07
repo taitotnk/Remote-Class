21d6858c5afb69860d6dea795e618e9c7a52ac06
ryuga <s1f101900903@iniad.org>
2021-12-07 11:48:53 +0900

    delete background color

--- src/components/IntroPage.tsx
+++ src/components/IntroPage.tsx
@@ -10,7 +10,7 @@ import { css } from '@emotion/react';
 const IntroPage = (props) => {
     return (
         <>
-        <div style={{position: 'relative', width:'100%',height:'100vh', backgroundColor: 'red'}}>
+        <div style={{position: 'relative', width:'100%',height:'100vh'}}>
             <Image src="/images/login_img/page1.png" alt="page1" layout='fill' />
 
             <div className={cssIntroPage.loginButtonDiv}>
@@ -23,27 +23,27 @@ const IntroPage = (props) => {
 
         </div>
 
-        <div style={{position: 'relative', width: '100%', height:'100vh', backgroundColor: 'red'}} id="page2">
+        <div style={{position: 'relative', width: '100%', height:'100vh'}} id="page2">
             <Image src="/images/login_img/page2.png" alt="page2" layout='fill' />
         </div>
 
-        <div style={{position: 'relative', width: '100%', height:'100vh', backgroundColor: 'red'}} id="page3">
+        <div style={{position: 'relative', width: '100%', height:'100vh'}} id="page3">
         <Image src="/images/login_img/page3.png" alt="page3" layout='fill'/>
         </div>
 
-        <div style={{position: 'relative', width: '100%', height:'100vh', backgroundColor: 'red'}} id="page4">
+        <div style={{position: 'relative', width: '100%', height:'100vh'}} id="page4">
         <Image src="/images/login_img/page4.png" alt="page4" layout='fill'/>
         </div>
 
-        <div style={{position: 'relative', width: '100%', height:'100vh', backgroundColor: 'red'}} id="page5">
+        <div style={{position: 'relative', width: '100%', height:'100vh'}} id="page5">
         <Image src="/images/login_img/page5.png" alt="page5" layout='fill'/>
         </div>
         
-        <div style={{position: 'relative', width: '100%', height:'100vh', backgroundColor: 'red'}} id="page6">
+        <div style={{position: 'relative', width: '100%', height:'100vh'}} id="page6">
         <Image src="/images/login_img/page6.png" alt="page6" layout='fill'/>
         </div>
 
-        <div style={{position: 'relative', width: '100%', height:'100vh', backgroundColor: 'red'}} id="page7">
+        <div style={{position: 'relative', width: '100%', height:'100vh'}} id="page7">
         <Image src="/images/login_img/page7.png" alt="page7" layout='fill'/>
         <div className={cssIntroPage.underLoginButtonDiv}>
             <Image src="/images/login_img/underloginbutton.png" alt="under_login_button" width={782} height={128} className={cssIntroPage.underLoginButton} onClick={login}/>

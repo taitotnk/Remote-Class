845622400236446c912c8fa737040914e41c6836
Taito-Code <taito.tanaka1211@gmail.com>
2021-12-15 00:19:00 +0900

    Feature: add title

--- src/pages/signIn.tsx
+++ src/pages/signIn.tsx
@@ -5,6 +5,7 @@ import login from "lib/login";
 import SignInHeader from "../components/SignInHeader";
 import IntroPage from "components/IntroPage";
 import cssHeader from "../styles/SignInHeader.module.css";
+import Head from "next/head";
 
 const SignIn: FC = () => {
   const { currentUser } = useAuth();
@@ -14,15 +15,21 @@ const SignIn: FC = () => {
   }, [currentUser]);
 
   return (
-    <div className={cssHeader.html}>
-      <SignInHeader></SignInHeader>
-      <div className={cssHeader.LoginButton}>
-        <button onClick={login}>INIADアカウントでログイン</button>
+    <>
+      <Head>
+        <title>Virtual Space for INIAD</title>
+        <link rel="shortcut icon" href="/images/icon.png" />
+      </Head>
+      <div className={cssHeader.html}>
+        <SignInHeader></SignInHeader>
+        <div className={cssHeader.LoginButton}>
+          <button onClick={login}>INIADアカウントでログイン</button>
+        </div>
+        <div className="scroll-main">
+          <IntroPage></IntroPage>
+        </div>
       </div>
-      <div className="scroll-main">
-        <IntroPage></IntroPage>
-      </div>
-    </div>
+    </>
   );
 };
 

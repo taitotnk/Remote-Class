ee238315f8649d9544f883dbd11cdf8dfb14b2e5
Taito-Code <taito.tanaka1211@gmail.com>
2022-01-10 22:57:37 +0900

    Update: update chat style

--- src/components/Fukidashi.tsx
+++ src/components/Fukidashi.tsx
@@ -1,5 +1,6 @@
 import style from "styles/fukidashi.module.css";
 import Image from "next/image";
+import { HStack } from "@chakra-ui/react";
 
 interface Props {
   text: string;
@@ -12,8 +13,16 @@ const Fukidashi = (props: Props) => {
   const chatValue = JSON.parse(props.text || "{}");
   return (
     <div className={style.fukidashi_container}>
-      <Image src={chatValue.img} width={40} height={40} alt={"chat_icon"} />
-      <div style={{ color: "black" }}>{chatValue.name}</div>
+      <HStack spacing="5px">
+        <Image
+          src={chatValue.img}
+          className="chat_icon"
+          width={30}
+          height={30}
+          alt={"chat_icon"}
+        />
+        <div style={{ color: "black" }}>{chatValue.name}</div>
+      </HStack>
       <div className={style.arrow_box}>
         {chatValue.text}
         <div className={style.arrow_box_after}></div>
@@ -28,6 +37,11 @@ const Fukidashi = (props: Props) => {
           fontSize: 40,
         }}
       ></div>
+      <style jsx global>{`
+        .chat_icon {
+          border-radius: 50%;
+        }
+      `}</style>
     </div>
   );
 };
